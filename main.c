fa30d42bb75ab6cfa2fd62f55666e26e93711adf
Ivo Meulemans <imeulema@c2r9s4.42lausanne.ch>
2024-12-04 12:30:25 +0100

    Simplification

--- main.c
+++ main.c
@@ -22,12 +22,11 @@ int	main(int ac, char **av)
 	a_id = 0;
 	while (a_id + 1 < ac)
 	{
-		if (check_arg_validity(av[a_id + 1]))
-			if (!fill_stack(a_stack, av[a_id + 1], a_id))
-				return (end_program_a_stack(a_stack, a_id));
+		if (check_arg_validity(av[a_id + 1])
+			&& fill_stack(a_stack, av[a_id + 1], a_id))
+			a_id++;
 		else
 			return (end_program_a_stack(a_stack, a_id));
-		a_id++;
 	}
 	if (!check_duplicates(a_stack, a_id))
 		return (end_program_a_stack(a_stack, a_id));
