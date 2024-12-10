c49a580101887b07ba179ae8e401eb4337edfc80
Ivo Meulemans <imeulema@c1r11s4.42lausanne.ch>
2024-12-10 16:07:26 +0100

    Copied everything back in place

---
+++ main.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imeulema <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 09:48:53 by imeulema          #+#    #+#             */
+/*   Updated: 2024/12/10 16:04:35 by imeulema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "incl/push_swap.h"
+
+void	print_stack(t_stack *stack)
+{
+	t_stack	*copy;
+	int		node;
+
+	copy = stack;
+	node = 0;
+	while (copy)
+	{
+		ft_printf("Node %d (%p): %d\n", node, copy, copy->content);
+		copy = copy->next;
+		node++;
+	}
+}
+
+int	main(int ac, char **av)
+{	
+	t_stack	*a_stack;
+	t_stack	*b_stack;
+
+	if (ac == 1)
+		return (1);
+	a_stack = parse_args(ac, av);
+	if (!a_stack || !b_stack)
+		return (1);
+	print_stack(a_stack);
+	return (0);
+}
